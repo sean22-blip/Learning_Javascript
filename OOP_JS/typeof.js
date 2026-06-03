9c5f849176533e6d62dc7016bf43c2f826da411e
sean22-blip <iseandxd@gmail.com>
2026-06-03 17:28:19 +0700

    typeof

---
+++ OOP_JS/typeof.js
@@ -0,0 +1,28 @@
+// console.log(typeof " ");
+// console.log(typeof 2);
+// console.log(typeof 12212.21);
+// console.log(typeof "Home work");
+// console.log(typeof true);
+// const a = Symbol("age");
+// const b = Symbol("age");
+// // console.log(a === b);
+// let q;
+// let m = defined;
+// let x = undefined;
+// let y = null;
+// console.log(q,x, y)
+// console.log
+let arr = [
+    {name: "isean",
+    age: 12,
+    id: 1
+    },
+    {
+    name: "mey",
+    age: 21,
+    id: 2
+    }
+]
+const id = Symbol("id");
+arr[id] = 21;
+console.log(Object.keys(arr));
