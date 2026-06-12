8e44679f5f0c04cda0eabc51705fe9f2459e172c
sean22-blip <iseandxd@gmail.com>
2026-06-12 15:52:07 +0700

    adding time stamp and saving it to file

--- connectTomySql/database.js
+++ connectTomySql/database.js
@@ -1,5 +1,7 @@
 import mysql from 'mysql2'
 import dotenv from 'dotenv'
+import fs from 'fs'
+// import { json } from 'express'
 dotenv.config()
 
 const pool = mysql.createPool({
@@ -17,8 +19,11 @@ async function getAirlines() {
 //     const [getId] = await pool.query(`select name from airlines where AirlineID = ${id}`)
 //     return getId
 // }
-async function getAirlineNameById(id){
-    // const id = Number(req.params.id);
+async function getAirlineNameById(id) {
+    const Numid = Number(id);
+    if (!Numid) {
+        return console.log("id must be provided!")
+    }
     // const [getId] = await pool.query(`select name from airliens where airlineid = ${id}`);
     // if(!id){
     //     return res.status(404).json({error: `id must be provided!`});
@@ -28,7 +33,7 @@ async function getAirlineNameById(id){
     //     return res.status(404).json({error: `${id} cannot be found!`})
     // }
     // const {id} = req.params;
-    pool.query(`select name from airlines where AirlineID = ?`, [id])
+    // pool.query(`select name from airlines where AirlineID = ?`, [id])
     // .then(([rows]) => {
     //     // return res.data;
     //     console.log(rows.data[0]);
@@ -40,16 +45,38 @@ async function getAirlineNameById(id){
     //     console.log({error: err.message})
     // })
     //  res.status(201).json(`successfully get airline name by id!`)
-    try{
-        const [rows] = await pool.query(`select name from airlines where AirlineID = ?`, [id])
+
+    try {
+        const [rows] = await pool.query(`select * from airlines where AirlineID = ?`, [id])
+        //this is called the prepared staement is when we are sending the sequel and the values to the database seperately
         // console.log(rows);
-        return rows[0];
-    }catch(err){
-        console.log({error: err.message});
+        // return rows[0];
+        // return rows[1];
+        // return rows[100];const findId = rows.find((r) => Numid === r.AirlineID);
+        const findId = rows.find((r) => Numid === r.AirlineID);
+        if (!findId) {
+            return console.log(`cannot find the id of ${Numid}`)
+        }
+        const time = new Date().toLocaleTimeString();
+        const date = new Date().toLocaleDateString();
+        const fullRow = JSON.stringify(rows, null , 2)
+        const jsonTmp = JSON.stringify(rows, ['Name'], 2)
+        fs.appendFileSync(`airlinesNameById.json`, `\n${date}${time}\n`);
+        fs.appendFileSync('airlinesNameById.json', jsonTmp);
+         fs.appendFileSync(`airlinesNameById.txt`, `\n === ${date} === ${time} ===\n`);
+        fs.appendFileSync('airlinesNameById.txt', jsonTmp);
+        console.log(fullRow);
+        // console.log(jsonTmp);
+        // console.log(`Added ${jsonTmp} \n To airliensNameById.json!`)
+        console.log(`Added ${jsonTmp} \n To airliensNameById.txt!`)
+        // return jsonTmp;
+    } catch (err) {
+        console.log({ error: err.message });
     }
 }
-const airlineName = await getAirlineNameById(1);
-console.log(airlineName);
+const airlineName = await getAirlineNameById(2);
+// console.log(airlineName);
+console.log("Thank you for using our system!")
 // const airlines = await getAirlines();
 // console.log(airlines);
 // const rows = result[0];
